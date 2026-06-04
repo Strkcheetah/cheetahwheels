df053832624bee2e96d3438faac13285307d3568
Strkcheetah <120026946+Strkcheetah@users.noreply.github.com>
2026-06-04 16:04:28 -0400

    Delete public directory

--- public/app.js
+++
@@ -1,5 +0,0 @@
-async function login(){
- const r=await fetch('/api/login',{method:'POST',headers:{'Content-Type':'application/json'},
- body:JSON.stringify({usuario:u.value,password:p.value})});
- msg.textContent=r.ok?'Login correcto':'Credenciales inválidas';
-}
