96e007f941d58ff2a1fca201e4d30a9bdec6fef0
Strkcheetah <120026946+Strkcheetah@users.noreply.github.com>
2026-06-04 16:12:05 -0400

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-function login(){
-const u=document.getElementById('user').value;
-const p=document.getElementById('pass').value;
-document.getElementById('msg').textContent=(u==='admin'&&p==='admin123')?'Login correcto':'Credenciales incorrectas';
-}
